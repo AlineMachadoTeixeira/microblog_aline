58de8ee35e16d898ad2af9b00c5e847d2694c4d9
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-10-27 10:49:47 -0300

    Fazendo o buscar funcionar

--- resultados.php
+++ resultados.php
@@ -1,47 +1,40 @@
-<?php 
+<?php
+
+use Microblog\Utilitarios;
+
 require_once "inc/cabecalho.php";
 $noticia->setTermo($_GET["busca"]);
+$resultado = $noticia->busca();
+//Utilitarios::dump($resultado)
 
 ?>
 
 
 <div class="row bg-white rounded shadow my-1 py-4">
     <h2 class="col-12 fw-light">
-        Você procurou por <span class="badge bg-dark"><?=$noticia->getTermo()?></span> e
-        obteve <span class="badge bg-info">X</span> resultados
+        Você procurou por <span class="badge bg-dark"><?= $noticia->getTermo() ?></span> e
+        obteve <span class="badge bg-info"><?= count($resultado) ?></span> resultados
     </h2>
-    
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
-                <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
-                </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
+    <?php foreach ($resultado as $itemNoticia) { ?>
+        <div class="col-12 my-1">
+            <article class="card">
+                <div class="card-body">
+                    <h3 class="fs-4 card-title fw-light"><?=$itemNoticia['titulo']?></h3>
+                    <p class="card-text">
+                        <time><?=$itemNoticia['data']?></time> -
+                        <?=$itemNoticia['resumo']?>
+                    </p>
 
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
-                <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
-                </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
+                    <a href="noticia.php?id=<?=$itemNoticia['id']?>" class="btn btn-primary btn-sm">Continuar lendo</a>
+                </div>
+            </article>
+        </div>
+    <?php } ?>
 
-</div>        
 
-<?php 
+
+</div>
+
+<?php
 require_once "inc/rodape.php";
 ?>
-
